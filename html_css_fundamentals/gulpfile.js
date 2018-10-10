a4171b221c7ff9d3e21fec37d131391fbafe375f
Anthony Flores <anthonyf@gmail.com>
2018-10-10 18:04:44 -0500

    Installed gulp-sass; added gupl-sass code

--- html_css_fundamentals/gulpfile.js
+++ html_css_fundamentals/gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp'),  
-  pug = require('gulp-pug');
+  pug = require('gulp-pug'),
+  sass = require('gulp-sass');
 
 // run this task by typing in gulp pug in CLI
 gulp.task('pug', function() {  
-  return gulp.src('pug_templates/**/*.pug')
+  return gulp.src('templates/pug/**/*.pug')
       .pipe(pug({
           pretty : true
       }
@@ -13,4 +14,16 @@ gulp.task('pug', function() {
 gulp.task('watch:pug', function(){
   return gulp.watch('pug_templates/**/*.pug', ['pug']);
 });
-gulp.task('default', ['watch:pug', 'pug']);
+
+var sassFiles = 'templates/sass/**/*.scss',
+    cssDest = 'styles/';
+gulp.task('sass', function(){
+    gulp.src(sassFiles)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(cssDest));
+});
+gulp.task('watch:sass', function(){
+  return gulp.watch(sassFiles, ['sass']);
+});
+
+gulp.task('default', ['watch:pug', 'pug', 'watch:sass', 'sass']);
