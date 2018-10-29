c7bac0550fa44244c1c2b9d60baab85dbae94d4b
Anthony Flores <anthonyf@gmail.com>
2018-10-29 06:39:15 -0500

    04_syntax_selectors; clean up

--- html_css_fundamentals/gulpfile.js
+++ html_css_fundamentals/gulpfile.js
@@ -2,28 +2,32 @@ var gulp = require('gulp'),
   pug = require('gulp-pug'),
   sass = require('gulp-sass');
 
+var pugFiles = 'templates/pug/**/*.pug',
+  htmlDest = '.'
 // run this task by typing in gulp pug in CLI
 gulp.task('pug', function() {  
-  return gulp.src('templates/pug/**/*.pug')
+  return gulp.src(pugFiles)
       .pipe(pug({
           pretty : true
       }
       )) // pipe to pug plugin
-      .pipe(gulp.dest('.')); // tell gulp our output folder
+      .pipe(gulp.dest(htmlDest)); // tell gulp our output folder
 });
 gulp.task('watch:pug', function(){
-  return gulp.watch('pug_templates/**/*.pug', ['pug']);
+  return gulp.watch(pugFiles, ['pug']);
 });
 
 var sassFiles = 'templates/sass/**/*.scss',
     cssDest = 'styles/';
 gulp.task('sass', function(){
     gulp.src(sassFiles)
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({
+          outputStyle: 'expanded'
+        }).on('error', sass.logError))
         .pipe(gulp.dest(cssDest));
 });
 gulp.task('watch:sass', function(){
   return gulp.watch(sassFiles, ['sass']);
 });
 
-gulp.task('default', ['watch:pug', 'pug', 'watch:sass', 'sass']);
+gulp.task('default', ['watch:sass', 'sass', 'watch:pug', 'pug']);
